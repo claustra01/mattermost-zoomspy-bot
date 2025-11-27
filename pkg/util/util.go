37837e1dde00979b2e15385958100e696da9503d
claustra01 <shinya430.g@gmail.com>
2025-11-27 14:10:58 +0900

    fix: ignore archive url

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -6,9 +6,10 @@ import (
 )
 
 var zoomURLRegex = regexp.MustCompile(`https?://[^\s]*zoom\.(us|com|gov)/[^\s]*`)
+var zoomArchiveRegex = regexp.MustCompile(`https?://[^\s]*zoom\.(us|com|gov)/(rec|recording)/[^\s]*`)
 
 func HasZoomURL(message string) bool {
-	return zoomURLRegex.MatchString(message)
+	return zoomURLRegex.MatchString(message) && !zoomArchiveRegex.MatchString(message)
 }
 
 func BuildPostURL(baseUrl string, postID string) string {
